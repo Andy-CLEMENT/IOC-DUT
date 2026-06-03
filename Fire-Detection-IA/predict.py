f3d2a41dab9049f95a4da17c5bcd9e9098447b74
Martini Alexis <alexismartini244@gmail.com>
2026-06-03 16:17:44 +0700

    finalisation ia

--- Fire-Detection-IA/predict.py
+++ Fire-Detection-IA/predict.py
@@ -1,26 +1,24 @@
 from ultralytics import YOLO
 
 def main():
-    # 1. Charger ton modèle fraîchement entraîné
-    # J'ai repris le chemin exact généré dans ton terminal tout à l'heure
-    model_path = "runs\\detect\\runs\\detect\\yolov8s_fire_day_IR_model-3\\weights\\best.pt"
-    
+    model_path = r"C:\Users\Alexis\Desktop\projet\IOC-DUT\Fire-Detection-IA\model_final\best.pt"
+
     print("Chargement de l'IA...")
     model = YOLO(model_path)
 
-    # 2. Choisir l'image ou la vidéo à tester
-    source_test = "test-fire7.jpg" # Modifie le nom si ta photo s'appelle autrement
+    dossier_test = "test_after_training"
 
-    # 3. Lancer la prédiction
-    print(f"Analyse de {source_test} en cours...")
+    print(f"Début de la boucle d'analyse sur le dossier : {dossier_test}")
+    
     results = model.predict(
-        source=source_test,
-        conf=0.4,       # Seuil de confiance : n'affiche que les détections > 40%
-        show=True,      # Ouvre une fenêtre pour afficher l'image annotée en direct
-        save=True       # Sauvegarde le résultat dans le dossier runs/detect/predict/
+        source=dossier_test,
+        conf=0.4,
+        show=True,
+        save=True
     )
 
-    print("Analyse terminée ! Regarde l'image.")
+    print("\nAnalyse terminée ! Toutes tes images annotées sont sauvegardées.")
+    print("Regarde dans le dossier : runs/detect/predict/")
 
 if __name__ == '__main__':
     main()
