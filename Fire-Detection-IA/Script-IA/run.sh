a7ee1c07ee317d3bbdc539cb18394ce244d339fd
Andy <153273761+Andy-CLEMENT@users.noreply.github.com>
2026-07-27 14:48:53 +0700

    Update run.sh

--- Fire-Detection-IA/Script-IA/run.sh
+++ Fire-Detection-IA/Script-IA/run.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
-
 cd "$(dirname "$0")"
 
-echo "--- System Startup ---"
+echo "--- running the system ---"
 
-echo "Wi-Fi Setup..."
-sudo iw dev wlan0 set power_save off
+echo "Wi-Fi configuration..."
+sudo iw dev wlP1p1s0 set power_save off
 
-echo "Port 8765 released..."
+echo "clean port 8765..."
 sudo fuser -k 8765/tcp > /dev/null 2>&1
 
-echo "Launching the relay server in the background..."
+echo "running server..."
 python3 serveur_relais.py > log_serveur.txt 2>&1 &
 SERVEUR_PID=$!
 
 sleep 2
 
-echo "Launching the AI (fire-detect.py)..."
-echo "Press 'q' in the video window to exit"
+echo "run IA (fire-detect.py)..."
+echo "press 'q' to quit."
 python3 fire-detect.py
 
-echo "Stopping now..."
+echo "switch off..."
 kill $SERVEUR_PID > /dev/null 2>&1
 sudo fuser -k 8765/tcp > /dev/null 2>&1
 
-echo "The system shut down properly"
+echo "program stopped."
